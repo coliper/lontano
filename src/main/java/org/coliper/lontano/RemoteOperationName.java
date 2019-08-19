abcd40f14d87c54f9dd59a0b9845797ac1b683be
Alexander Einhaeuser <extern.einhaeuser_alexander@allianz.de>
2019-08-19 14:15:23 +0200

    renamed some classes and added RemoteOperation class

---
+++ src/main/java/org/coliper/lontano/RemoteOperationName.java
@@ -0,0 +1,10 @@
+package org.coliper.lontano;
+
+public class RemoteOperationName extends StringValueObject {
+
+	public RemoteOperationName(String value) {
+		super(value);
+		// TODO Auto-generated constructor stub
+	}
+
+}
